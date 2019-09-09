846b92d0e5ea627f1b9087277d6162f062f501fe
Akira <autumn.no7@gmail.com>
2019-09-09 15:39:32 +0900

    fix:Bootstrap修正

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
+gem 'bootstrap', '~> 4.0.0.alpha6'
+# gem 'bootstrap', '~> 4.3.1'
+
 
 # Backend
 gem 'jbuilder', '~> 2.5'
@@ -24,7 +27,6 @@ gem 'bcrypt', '3.1.11'
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'jquery-rails'
-gem 'bootstrap', '~> 4.3.1'
 
 
 group :development, :test do
---
+++ app/assets/javascripts/bootstrap-sprockets.js
@@ -0,0 +1,12 @@
+//= require ./bootstrap/util
+//= require ./bootstrap/scrollspy
+//= require ./bootstrap/button
+//= require ./bootstrap/collapse
+//= require ./bootstrap/modal
+//= require ./bootstrap/dropdown
+//= require ./bootstrap/carousel
+//= require ./bootstrap/toast
+//= require ./bootstrap/tooltip
+//= require ./bootstrap/popover
+//= require ./bootstrap/alert
+//= require ./bootstrap/tab
