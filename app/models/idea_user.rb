4527b96553efa4dc90bff2936ff858f00813001b
yuji hirai <yujihirai0420@gmail.com>
2019-09-18 15:33:40 +0900

    Add:ideaモデルとidea_userモデルをselect_genre_in_ideaモデルでアソシエーション。idea_userのseedデータの作成

--- app/models/idea_user.rb
+++ app/models/idea_user.rb
@@ -1,2 +1,4 @@
 class IdeaUser < ApplicationRecord
+  has_many :select_genre_in_ideas, dependent: :destroy
+  has_many :ideas, through: :select_genre_in_ideas
 end
--- app/models/select_genre_in_idea.rb
+++ app/models/select_genre_in_idea.rb
@@ -1,2 +1,4 @@
 class SelectGenreInIdea < ApplicationRecord
+  belongs_to :idea
+  belongs_to :idea_user
 end
