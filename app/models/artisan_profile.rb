2a6d280ff2855b8dea079c1770ece86bc01818b3
Akira <autumn.no7@gmail.com>
2019-09-21 23:16:31 +0900

    fix:artisanProfileモデル修正

--- app/models/artisan_profile.rb
+++ app/models/artisan_profile.rb
@@ -1,3 +1,5 @@
 class ArtisanProfile < ApplicationRecord
+  has_many :select_genre_in_artisans, inverse_of: :artisan_profile, dependent: :destroy
+  has_many :artisan_genre_lists, through: :select_genre_in_artisans
   belongs_to :user, inverse_of: :artisan_profile
 end
