f53a878dd1f3c64aa467616851e62df40529c7a0
Akira <autumn.no7@gmail.com>
2019-09-23 19:23:45 +0900

    fix:管理画面、モデルアソシエーション調整

--- app/models/artisan_genre_list.rb
+++ app/models/artisan_genre_list.rb
@@ -1,4 +1,4 @@
 class ArtisanGenreList < ApplicationRecord
-  has_many :artisan_profiles, through: :select_genre_in_artisans
   has_many :select_genre_in_artisans, dependent: :destroy
+  has_many :artisan_profiles, through: :select_genre_in_artisans
 end
--- app/models/artisan_processing_list.rb
+++ app/models/artisan_processing_list.rb
@@ -1,4 +1,4 @@
 class ArtisanProcessingList < ApplicationRecord
-  has_many :artisan_profiles, through: :select_processing_in_artisans
   has_many :select_processing_in_artisans, dependent: :destroy
+  has_many :artisan_profiles, through: :select_processing_in_artisans
 end
