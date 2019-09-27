528e50c65743282a1bf1e953970b87d90e241a7d
setsuotokohorikawa <ralralph@gmail.com>
2019-09-27 14:36:29 +0900

    Fix:projectのイイネのエラー

--- app/controllers/project_likes_controller.rb
+++ app/controllers/project_likes_controller.rb
@@ -2,11 +2,11 @@ class ProjectLikesController < ApplicationController
 
   def create
     like = current_user.project_likes.create(project_id: params[:project_id])
-    ridirect_to project_url, notice: "プロジェクトをお気に入り登録しました！"
+    redirect_to project_url(id: params[:project_id]), notice: "プロジェクトをお気に入り登録しました！"
   end
 
   def destroy
-    like = current_user.project_likes.find_by(id: params[:id]).destory
-    ridirect_to project_url, notice: "お気に入りを解除しました。"
+    like = current_user.project_likes.find_by(id: params[:project_id]).destroy
+    redirect_to project_url(id: params[:id]), notice: "お気に入りを解除しました。"
   end
 end
