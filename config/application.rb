58538311c4e4afdc13d30d04fbae51bde0883e95
setsuotokohorikawa <ralralph@gmail.com>
2019-09-29 21:26:48 +0900

    fix:Time zoneを日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,9 @@ module Brandfunding
     config.load_defaults 5.2
 
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
   end
 end
