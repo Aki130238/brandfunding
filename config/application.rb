04e50e6d4044361eca462799ae43aeb9f770b23b
Aki130238 <autumn.no7@gmail.com>
2019-09-29 18:43:11 +0900

    fix:タイムゾーン変更

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ Bundler.require(*Rails.groups)
 module Brandfunding
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.time_zone = 'Asia/Tokyo'
 
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
