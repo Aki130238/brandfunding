180ebce6b8a8c5900406159131fe163c3313d6ff
yuji hirai <yujihirai0420@gmail.com>
2019-09-30 11:06:42 +0900

    ideaモデルに数値、文字の入力数制限の設定

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -4,20 +4,13 @@ class Idea < ApplicationRecord
   has_many :idea_comments, dependent: :destroy
   belongs_to :user
 
-  validates :idea_title, presence: true
-  validates :idea_amount, presence: true
-  validates :idea_about, presence: true
-  validates :idea_usage, presence: true
-  validates :idea_commit, presence: true
-  validates :product_image, presence: true
-  validates :idea_category, presence: true
-  validates :idea_category_details, presence: true
-  validates :idea_material, presence: true
-  validates :work_style, presence: true
-  validates :delivery_date, presence: true
-  validates :budget, presence: true
-  validates :idea_user_ids, presence: true
-
+  validates :idea_title, presence: true, length: { in: 1..40 }
+  validates :idea_amount, presence: true, numericality: { greater_than: 0, less_than: 10000 }
+  validates :idea_about, presence: true, length: { in: 1..1000 }
+  validates :idea_commit, presence: true, length: { in: 1..1000 }
+  validates :idea_usage, :product_image, :idea_category,
+            :idea_category_details, :idea_material, :work_style,
+            :delivery_date, :budget, :idea_user_ids, presence: true
   validate :delivery_date_cannot_be_in_the_past
 
   def delivery_date_cannot_be_in_the_past
--- app/models/idea_comment.rb
+++ app/models/idea_comment.rb
@@ -1,4 +1,5 @@
 class IdeaComment < ApplicationRecord
   belongs_to :idea
   belongs_to :user
+  validates :content, presence: true, length: { in: 1..1000 }
 end
