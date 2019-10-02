1ca237979ce7485c326e1b93368eb67a86166776
yuji hirai <yujihirai0420@gmail.com>
2019-10-02 14:20:23 +0900

    return機能実装。projectのindex, showページのスタイリング

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_072917) do
+ActiveRecord::Schema.define(version: 2019_10_02_043306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -279,6 +279,27 @@ ActiveRecord::Schema.define(version: 2019_10_01_072917) do
     t.datetime "updated_at", null: false
     t.index ["artisan_processing_list_id"], name: "index_artisan_processing_list_id"
     t.index ["artisan_profile_id"], name: "index_artisan_processing_id"
+  end
+
+  create_table "sponsers", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "project_return_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sponsor_lists", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "project_return_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sponsors", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "project_return_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_profiles", force: :cascade do |t|
