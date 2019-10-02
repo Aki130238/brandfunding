34e9c22d73872400d80049003b89bc80460d0d26
yuji hirai <yujihirai0420@gmail.com>
2019-10-02 17:37:38 +0900

    return詳細ページの修正

--- app/controllers/project_returns_controller.rb
+++ app/controllers/project_returns_controller.rb
@@ -1,11 +1,12 @@
 class ProjectReturnsController < ApplicationController
 
   def create
+    @project = Project.find(params[:id])
     @project_return = ProjectReturn.new(project_return_params)
     @project_return.project_id = params[:id]
     # @project_return = @project.project_returns.build(project_return_params)
     if @project_return.save
-      redirect_to root_path
+      redirect_to project_path(@project)
     else
       redirect_to root_path
     end
