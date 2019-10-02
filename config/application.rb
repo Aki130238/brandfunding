d01c5cf0619fd24892665c5b5ac86f64c6235591
Aki130238 <autumn.no7@gmail.com>
2019-10-02 13:20:50 +0900

    add:辞書追加

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Brandfunding
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
   end
 end
---
+++ config/initializers/locale.rb
@@ -0,0 +1,3 @@
+I18n.config.available_locales = :ja
+I18n.default_locale = :ja
+Rails.application.config.i18n.default_locale = :ja
