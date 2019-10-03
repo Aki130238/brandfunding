7ae2d4266ee439f4e9cfbe05fca9413defb72df6
setsuotokohorikawa <ralralph@gmail.com>
2019-10-03 13:57:52 +0900

    add:aritsan_profileにimageカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_043306) do
+ActiveRecord::Schema.define(version: 2019_10_03_045122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_10_02_043306) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "artisan_image"
     t.index ["user_id"], name: "index_artisan_profiles_on_user_id"
   end
 
