e0e4a01e65edf98ad215ee381d2056e55f420b50
yuji hirai <yujihirai0420@gmail.com>
2019-10-03 13:58:50 +0900

    idea_commentにnull制約追加

--- app/controllers/idea_comments_controller.rb
+++ app/controllers/idea_comments_controller.rb
@@ -4,7 +4,7 @@ class IdeaCommentsController < ApplicationController
     @idea = Idea.find(params[:idea_id])
     @idea_comment = @idea.idea_comments.build(idea_comment_params)
     @idea_comment.user_id = current_user.id
-    redirect_to idea_path(@idea), notice: "コメントしました。" if @idea_comment.save
+    redirect_to idea_path(@idea), notice: "コメントしました" if @idea_comment.save
   end
 
   def destroy
@@ -12,9 +12,9 @@ class IdeaCommentsController < ApplicationController
     @idea_comment = IdeaComment.find(params[:id])
     if @idea_comment.user == current_user || @idea.user == current_user
       @idea_comment.destroy
-      redirect_to idea_path(@idea), notice: "コメントを削除しました。"
+      redirect_to idea_path(@idea), notice: "コメントを削除しました"
     else
-      redirect_to idea_path(@idea), notice: "コメントの削除に失敗しました。"
+      redirect_to idea_path(@idea), notice: "コメントの削除に失敗しました"
     end
   end
 
