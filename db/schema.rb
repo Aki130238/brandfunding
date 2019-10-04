a937fc549fc0c8321079c56f7862fbc505d9634d
yuji hirai <yujihirai0420@gmail.com>
2019-10-04 14:48:56 +0900

    projectsにバリデーション設定。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_171055) do
+ActiveRecord::Schema.define(version: 2019_10_04_054657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -212,8 +212,8 @@ ActiveRecord::Schema.define(version: 2019_10_03_171055) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string "project_title"
-    t.text "project_text"
+    t.string "project_title", null: false
+    t.text "project_text", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
