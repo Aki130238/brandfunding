8099debc397738eaa697b17bdec10b3b08e14602
yuji hirai <yujihirai0420@gmail.com>
2019-10-04 16:17:14 +0900

    usersにバリデーション設定。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_060822) do
+ActiveRecord::Schema.define(version: 2019_10_04_071529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -311,13 +311,14 @@ ActiveRecord::Schema.define(version: 2019_10_04_060822) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "user_status", default: 3
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "artisan_profiles", "users"
