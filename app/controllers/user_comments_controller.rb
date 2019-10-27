471c4c56476763b34b0dd85478eab718d4cec96b
Aki130238 <autumn.no7@gmail.com>
2019-10-27 20:21:36 +0900

    fix:コメント機能Ajax実装完了

--- app/controllers/user_comments_controller.rb
+++ app/controllers/user_comments_controller.rb
@@ -3,13 +3,24 @@ class UserCommentsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @user_comment = @user.user_comments.build(user_comment_params)
+    @user_comments = @user.user_comments
+
+    # what: リクエストに明示的にフォーマットの指定が無かった場合はindexアクションのデフォルトのテンプレートが呼び出されて結果が返されますが、リクエストのフォーマットが指定されていた場合、それが「json」なら「render :json => オブジェクト」と返し、「xml」なら「render :xml => オブジェクト」を使って返します。
+    # why: requestという変数は実はcontorllerだとどこでも使える そのうちのプロパティーの一つにformatという物が入ってる format
+    # how: content-type (curl)はrailsならデフォでhtml　ただjsの時もある　そういう時に返したいデータを変えたい そういう時にrespond_toはつかうもの
     respond_to do |format|
       if @user_comment.save
         format.js { render :index }
+        format.html { redirect_to user_path(@user) }
       else
+        format.js { render :index, notice: '投稿できませんでした...' }
         format.html { redirect_to user_path(@user), notice: '投稿できませんでした...' }
       end
     end
+    # なぜviewファイルに行くのか
+    # what: そもそもviewとは
+    # why: 
+    # how(つまり): 
   end
 
   def show
