4778d30e75db636b74159b5d926430d121e2dbb9
Aki130238 <autumn.no7@gmail.com>
2019-10-30 12:28:11 +0900

    fix:talkroom空コメント調整

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
+  validates :body, presence: true, length: { in: 1..1000 }
 
   validates_presence_of :body, :conversation_id, :user_id
   def message_time
